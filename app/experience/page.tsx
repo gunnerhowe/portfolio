ac5f72418b505f24fced92636fa71f33ff4b0a9d
gunnerhowe <gunnerlevihowe@gmail.com>
2024-06-13 17:25:12 -0500

    Update

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <section>
       <h2 className="font-semibold text-2xl mb-1 tracking-tighter">Skills</h2>
-      <p className="mb-8">Python, JavaScript, API, Node.js, Next.js, React, Pandas, Keras, SQL, MongoDB, Azure, TensorFlow, PowerShell, NumPy, Machine Learning, Power Automate, Automated Web Engine, Puppet, Azure DevOps, AWS S3, Bcrypt.js, Next-Auth, RESTful API, Google Cloud Platform, AWS, HTML5, CSS3, Selenium, Appium, Pytest, Docker, Kubernetes, CI/CD (Azure DevOps, Jenkins), GitHub, Vercel, Nginx, WireShark, npm, pnpm, Tailwind, Typescript</p>
+      <p className="mb-8">Python, JavaScript, API, Node.js, Next.js, React, Pandas, Keras, SQL, MongoDB, Azure, TensorFlow, PowerShell, NumPy, Machine Learning, Power Automate, Automated Web Engine, Puppet, Azure DevOps, AWS S3, Bcrypt.js, Next-Auth, RESTful API, Google Cloud Platform, AWS, HTML5, CSS3, Selenium, Appium, Pytest, Docker, Kubernetes, CI/CD (Azure DevOps, Jenkins), GitHub, Vercel, Nginx, WireShark, npm, pnpm, Tailwind, Typescript, Truffle, web3, Solidify, Smart Contracts</p>
       <h2 className="font-semibold text-2xl mb-1 tracking-tighter">Experience</h2>
       <ExperiencePosts />
     </section>
