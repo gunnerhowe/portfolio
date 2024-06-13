6128729c323075bcfcafc8c5985ca9a6d765e3c5
gunnerhowe <gunnerlevihowe@gmail.com>
2024-06-12 22:20:51 -0500

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Page() {
         Welcome
       </h1>
       <p className="mb-4">
-        {`My name is Gunner Howe, I'm a full-stack developer based in Arkansas, USA. I have extensive experience in AI integration, front-end development, back-end developent, APIs and automation`}
+        {`My name is Gunner Howe, I'm a full-stack developer based in Little Rock Arkansas. I have extensive experience in AI integration, front-end development, back-end developent, APIs and automation`}
       </p>
       <p className="mb-4">
-        {`Innovative and highly motivated Software Engineer with extensive experience in cloud computing, Linux systems, and automated testing frameworks. Seeking to leverage expertise in performance optimization, system integration, and automation to contribute to Rivian’s Connectivity Software team, driving the development and validation of cutting-edge connectivity features.`}
+        {`Innovative and highly motivated Software Engineer with extensive experience in cloud computing, Linux systems, and automated testing frameworks. Seeking to leverage expertise in performance optimization, system integration, and automation to contribute to an innovative Software team, driving the development and validation of cutting-edge features.`}
       </p>
       <div className="my-8">
       </div>
